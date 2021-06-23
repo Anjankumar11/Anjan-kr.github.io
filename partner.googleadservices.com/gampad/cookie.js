f575ad241554a6497cef40ece2c8cdedb1aa01c3
Anjankumar11 <86356710+Anjankumar11@users.noreply.github.com>
2021-06-23 21:15:49 +0530

    webpages

---
+++ partner.googleadservices.com/gampad/cookie.js
@@ -0,0 +1,8 @@
+_gfp_s_({
+    "_cookies_": [{
+        "_value_": "ID=2a3eeea377d2079e-22cc98c442c900f8:T=1623137142:RT=1623137142:S=ALNI_MaO52ipRu5SagUncw7QZum7gE8E-g",
+        "_expires_": 1656833142,
+        "_path_": "/",
+        "_domain_": "github.io"
+    }]
+});
