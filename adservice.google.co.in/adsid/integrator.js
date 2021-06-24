da980f47511749c65a6e4c3991543168d199b336
Anjankumar11 <86356710+Anjankumar11@users.noreply.github.com>
2021-06-24 13:35:50 +0530

    webpages

--- adservice.google.co.in/adsid/integrator.js
+++
@@ -1,7 +0,0 @@
-processGoogleToken({
-    "newToken": "ChAI8Kr8hQYQueHGxqj0_a0TEj0A9cGHIlsSLprrLyDO3VuTbpLkZbZpe5hunD6A-6Ov5QuLTDNk5wx_jMoF8cMWhwJKBSLuM6xl03utoj2H",
-    "validLifetimeSecs": 300,
-    "freshLifetimeSecs": 300,
-    "1p_jar": "2021-05-12-12",
-    "pucrd": ""
-});
